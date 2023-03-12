2b0ed1be27d233c958e39f2ab36a7ab5c997872f
filipchukdv <114296789+filipchukdv@users.noreply.github.com>
2023-03-12 17:13:40 +0000

    Deploying to gh-pages from @ filipchukdv/goit-js-hw-09@14a412b0f5da0533867caced4593f74005c9004a 🚀

--- 01-color-switcher.83e14b8a.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),d=document.querySelector("body");function n(){d.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}e.disabled=!0,t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1;setInterval(n,1e3)})),e.addEventListener("click",(function(){t.disabled=!1,e.disabled=!0,clearInterval(timerId)}))}();
-//# sourceMappingURL=01-color-switcher.83e14b8a.js.map
